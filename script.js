86cf3b9e5106e6dbeee2832ed452cd3428e891c3
Stephen Edward Saragih <118141187+stephensaragih@users.noreply.github.com>
2024-04-07 03:58:23 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Initialize Typed.js for typing animation
+var typed = new Typed('#typed', {
+    strings: ["Freelancer", "Developer", "Designer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true
+});
